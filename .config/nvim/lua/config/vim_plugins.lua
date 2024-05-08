381aaf3702a29b10d681663a27f19ce2d213e05a
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-05-09 03:34:23 +0900

    nvim: 移動関連のvimプラグインを追加
    comfortable-motion.vim: <C-d>や<C-u>で滑らかなスクロールを実現するプラグイン

--- .config/nvim/lua/config/vim_plugins.lua
+++ .config/nvim/lua/config/vim_plugins.lua
@@ -1,1 +1,7 @@
 vim.opt.packpath:append { "~/.config/nvim" }
+
+-- ~/.config/nvim/pack/*/opt/ にvimプラグインのディレクトリを配置して、
+-- ディレクトリ名を指定することで、ロードできる。
+-- pack/*/start/* に配置しても自動でロードされなかった。
+-- 参考: https://neovim.io/doc/user/repeat.html#packages
+vim.cmd("packadd comfortable-motion.vim")
