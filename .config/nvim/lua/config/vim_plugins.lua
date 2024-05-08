e18467e6bf651e9708ec2682599128b574748f17
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-05-09 03:46:15 +0900

    nvim: コメントを変更

--- .config/nvim/lua/config/vim_plugins.lua
+++ .config/nvim/lua/config/vim_plugins.lua
@@ -1,7 +1,9 @@
-vim.opt.packpath:append { "~/.config/nvim" }
-
 -- ~/.config/nvim/pack/*/opt/ にvimプラグインのディレクトリを配置して、
 -- ディレクトリ名を指定することで、ロードできる。
 -- pack/*/start/* に配置しても自動でロードされなかった。
 -- 参考: https://neovim.io/doc/user/repeat.html#packages
+vim.opt.packpath:append { "~/.config/nvim" }
+
+-- 移動系: <C-d>や<C-u>などで滑らかなスクロースを実現する
+-- https://github.com/yuttie/comfortable-motion.vim
 vim.cmd("packadd comfortable-motion.vim")
