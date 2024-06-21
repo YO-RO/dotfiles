7bc286968b244820a8ccb3b6f3a573ca540ac189
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-06-21 17:53:22 +0900

    nvim: coding用のプラグインを追加

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,5 +1,6 @@
 return {
     {
+        -- move
         "folke/flash.nvim",
         event = "VeryLazy",
         opts = {
@@ -23,5 +24,95 @@ return {
         version = "*", -- Use for stability; omit to use `main` branch for the latest features
         event = "VeryLazy",
         config = true,
+    },
+    {
+        "numToStr/Comment.nvim",
+        lazy = false,
+        opts = {
+            toggler = {
+                ---Line-comment toggle keymap
+                line = "<leader>/",
+                ---Block-comment toggle keymap
+                block = "<leader>cb",
+            },
+            opleader = {
+                ---Line-comment keymap
+                line = "<leader>/",
+                ---Block-comment keymap
+                block = "<leader>cb",
+            },
+            extra = {
+                ---Add comment on the line above
+                above = "<leader>cO",
+                ---Add comment on the line below
+                below = "<leader>co",
+                ---Add comment at the end of line
+                eol = "<leader>cA",
+            },
+        },
+    },
+    {
+        -- ネストの展開・結合
+        "Wansmer/treesj",
+        keys = { "<space>m", "<space>j", "<space>s" },
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        opts = {},
+    },
+    {
+        "monaqa/dial.nvim",
+        config = function()
+            local augend = require("dial.augend")
+            require("dial.config").augends:register_group({
+                default = {
+                    augend.integer.alias.decimal,
+                    augend.integer.alias.hex,
+                    augend.constant.alias.bool,
+                    augend.date.alias["%Y/%m/%d"],
+                    augend.date.alias["%Y-%m-%d"],
+                    augend.date.alias["%H:%M"],
+                    augend.date.alias["%Y年%-m月%-d日"],
+                    augend.date.alias["%Y年%-m月%-d日(%ja)"],
+                    augend.constant.alias.ja_weekday,
+                    augend.constant.alias.ja_weekday_full,
+                },
+            })
+        end,
+        keys = {
+            {
+                "<C-a>",
+                function()
+                    require("dial.map").manipulate("increment", "normal")
+                end,
+                desc = "Open file explorer",
+            },
+            {
+                "<C-a>",
+                function()
+                    require("dial.map").manipulate("increment", "visual")
+                end,
+                mode = "v",
+                desc = "Open file explorer",
+            },
+            {
+                "<C-x>",
+                function()
+                    require("dial.map").manipulate("decrement", "normal")
+                end,
+                desc = "Open file explorer",
+            },
+            {
+                "<C-x>",
+                function()
+                    require("dial.map").manipulate("decrement", "visual")
+                end,
+                mode = "v",
+                desc = "Open file explorer",
+            },
+        },
+    },
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = true
     },
 }
