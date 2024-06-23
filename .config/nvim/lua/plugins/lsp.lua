76fbc965299a462e24ecb44aab16ff372402294e
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-06-24 00:27:12 +0900

    nvim: style

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -50,21 +50,66 @@ return {
                 group = vim.api.nvim_create_augroup('UserLspConfig', {}),
                 callback = function(ev)
                     local bufopts = { noremap = true, silent = true, buffer = ev.buf }
-                    vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format,
-                        vim.tbl_extend("force", bufopts, { desc = "Format current buffer" }))
-                    vim.keymap.set("n", "<leader>lR", "<cmd>Lspsaga rename<CR>", bufopts)
 
-                    vim.keymap.set("n", "<leader>lr", "<cmd>Lspsaga finder ref<CR>", bufopts)
-                    vim.keymap.set("n", "<leader>li", "<cmd>Lspsaga finder imp<CR>", bufopts)
+                    vim.keymap.set(
+                        "n", "<leader>lf",
+                        vim.lsp.buf.format,
+                        vim.tbl_extend("force", bufopts,
+                            { desc = "Format current buffer" })
+                    )
+                    vim.keymap.set(
+                        "n", "<leader>lR",
+                        "<cmd>Lspsaga rename<CR>",
+                        bufopts
+                    )
 
-                    vim.keymap.set("n", "K", "<cmd>Lspsaga peek_definition<CR>", bufopts)
-                    vim.keymap.set("n", "<C-]>", "<cmd>Lspsaga goto_definition<CR>", bufopts)
+                    vim.keymap.set(
+                        "n", "<leader>lr",
+                        "<cmd>Lspsaga finder ref<CR>",
+                        bufopts
+                    )
+                    vim.keymap.set(
+                        "n", "<leader>li",
+                        "<cmd>Lspsaga finder imp<CR>",
+                        bufopts
+                    )
 
-                    vim.keymap.set("n", "<leader>la", "<cmd>Lspsaga code_action<CR>", bufopts)
-                    vim.keymap.set("n", "<leader>ld", "<cmd>Lspsaga show_line_diagnostics<CR>", bufopts)
-                    vim.keymap.set("n", "<leader>lD", "<cmd>Lspsaga show_buf_diagnostics<CR>", bufopts)
-                    vim.keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", bufopts)
-                    vim.keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", bufopts)
+                    vim.keymap.set(
+                        "n", "K",
+                        "<cmd>Lspsaga peek_definition<CR>",
+                        bufopts
+                    )
+                    vim.keymap.set(
+                        "n", "<C-]>",
+                        "<cmd>Lspsaga goto_definition<CR>",
+                        bufopts
+                    )
+
+                    vim.keymap.set(
+                        "n", "<leader>la",
+                        "<cmd>Lspsaga code_action<CR>",
+                        bufopts
+                    )
+                    vim.keymap.set(
+                        "n", "<leader>ld",
+                        "<cmd>Lspsaga show_line_diagnostics<CR>",
+                        bufopts
+                    )
+                    vim.keymap.set(
+                        "n", "<leader>lD",
+                        "<cmd>Lspsaga show_buf_diagnostics<CR>",
+                        bufopts
+                    )
+                    vim.keymap.set(
+                        "n", "[d",
+                        "<cmd>Lspsaga diagnostic_jump_prev<CR>",
+                        bufopts
+                    )
+                    vim.keymap.set(
+                        "n", "]d",
+                        "<cmd>Lspsaga diagnostic_jump_next<CR>",
+                        bufopts
+                    )
                 end,
             })
         end,
