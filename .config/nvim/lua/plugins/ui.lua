09dc34d94398898984f3cd4c4af080dfa974df1c
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-06-25 00:40:15 +0900

    nvim: lualineのUIを調整
    inactive_sectionsにはfilenameのみを表示するように変更

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -166,6 +166,14 @@ return {
                 lualine_y = { 'progress' },
                 lualine_z = {},
             },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { 'filename' },
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {},
+            },
         },
         dependencies = { 'nvim-tree/nvim-web-devicons' }
     },
